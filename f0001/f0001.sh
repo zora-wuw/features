ceb616b6193b7d2a42a2e2d0b500cfaac1b1d024
Amir Aryani <amir.aryani@gmail.com>
2019-04-22 09:06:10 +1000

    added mongorestore to f1

--- f0001/f0001.sh
+++ f0001/f0001.sh
@@ -3,7 +3,6 @@ aws s3 sync s3://$1 .
 FILES=./*
 for f in $FILES
 do
-  echo "Processing $f file..."
-  # take action on each file. $f store current file name
-
+ 	echo "Processing $f file..."
+	mongorestore --gzip --archive=$f  -v
 done
