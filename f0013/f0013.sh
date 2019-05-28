35d3a533cffa028553b48fa4ac191d86f8be18b0
chrisliiii <38777606+chrisliiii@users.noreply.github.com>
2019-05-28 15:46:32 +1000

    Update f0013.sh

--- f0013/f0013.sh
+++ f0013/f0013.sh
@@ -5,7 +5,6 @@ input_folder=$1
 start_name=$2
 output_file=$3
 
-cd $input_folder/
 # concat all csv files
 # only keep the header of the first file if all files have the same header
-awk 'FNR==1 && NR!=1{next;}{print}' $start_name*.csv >> $output_file
+awk 'FNR==1 && NR!=1{next;}{print}' $input_folder/$start_name*.csv >> $output_file
