039b0cdb2357c5aaa028770df6d282fd21ee6556
Chris <chrislishuanghong@gmail.com>
2019-07-05 18:38:22 +1000

    update test version

--- f0014/f0014-a.sh
+++ f0014/f0014-a.sh
@@ -10,10 +10,7 @@ for file in $folder/*2019_W14_Twitter_Australia*.gz; do
   mongorestore --gzip --archive=$file \
   && count=`mongo $db_name --eval "printjson(db.$filename.count());" --quiet` \
   && index=`mongo $db_name --eval "printjson(db.$filename.getIndexes());" --quiet` \
-  && ind=$(echo $index | jq -r '.[].name') \
-  && indexname=$(echo $ind | tr ' ' ';') \
-  && echo "$filename, $count,$indexname" \
-  && echo "$filename, $count,$indexname" >> $output_file \
+  && echo "$filename, $count,$index" >> $output_file \
   && mongo $db_name --eval "db.$filename.drop()"
 
   #echo $ind | tr ' ' ','
