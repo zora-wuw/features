d3e695a2e8f47abaafa91a52041a276e03b5f378
Chris <chrislishuanghong@gmail.com>
2019-07-05 18:27:28 +1000

    test version

--- f0014/f0014-a.sh
+++ f0014/f0014-a.sh
@@ -4,7 +4,7 @@ folder=$1
 db_name=$2
 output_file=$3
 
-for file in $folder/*Australia*.gz; do
+for file in $folder/*2019_W14_Twitter_Australia*.gz; do
   filename=$(basename -- "$file")
   filename="${filename%.*}"
   mongorestore --gzip --archive=$file \
@@ -12,6 +12,7 @@ for file in $folder/*Australia*.gz; do
   && index=`mongo $db_name --eval "printjson(db.$filename.getIndexes());" --quiet` \
   && ind=$(echo $index | jq -r '.[].name') \
   && indexname=$(echo $ind | tr ' ' ';') \
+  && echo "$filename, $count,$indexname" \
   && echo "$filename, $count,$indexname" >> $output_file \
   && mongo $db_name --eval "db.$filename.drop()"
 
