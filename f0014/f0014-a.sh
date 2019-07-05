fb110d083b60984b40afa8c0c0a4d0edbdd9355e
Chris <chrislishuanghong@gmail.com>
2019-07-05 18:56:17 +1000

    update

--- f0014/f0014-a.sh
+++ f0014/f0014-a.sh
@@ -5,11 +5,10 @@ db_name=$2
 output_file=$3
 
 for file in $folder/*Australia*.gz; do
-  echo "$file"
   filename=$(basename -- "$file")
   filename="${filename%.*}"
   subfilename="${filename#*mongodb_}"
-  mongorestore --gzip --archive=$filename \
+  mongorestore --gzip --archive=$file \
   && count=`mongo $db_name --eval "printjson(db.$subfilename.count());" --quiet` \
   && index=`mongo $db_name --eval "printjson(db.$subfilename.getIndexes());" --quiet` \
   && echo "$filename, $count,$index" >> $output_file \
