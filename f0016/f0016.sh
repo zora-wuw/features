fe892ef86d29f7cab8e589579e3fe2240669be38
Chris <chrislishuanghong@gmail.com>
2019-07-05 17:58:55 +1000

    upload f16

---
+++ f0016/f0016.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+folder=$1
+db_name=$2
+
+for file in $folder/*.gz; do
+  filename=$(basename -- "$file")
+  filename="${filename%.*}"
+  mongorestore --gzip --archive=$file \
+  && python3 f0016.py $filename \
+  && mongo $db_name --eval "db.$filename.drop()"
+done
