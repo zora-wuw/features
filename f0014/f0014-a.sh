08ec311c742e1fb0255306943ded52c3cda59917
Chris <chrislishuanghong@gmail.com>
2019-07-05 21:26:41 +1000

    debugging

--- f0014/f0014-a.sh
+++ f0014/f0014-a.sh
@@ -4,20 +4,25 @@ folder=$1
 db_name=$2
 output_file=$3
 
-for file in $folder/*mongodb_2019_W14_Twitter_Australia*.gz; do
-  filename=$(basename -- "$file")
-  filename="${filename%.*}"
-  filename="${filename#*mongodb_}"
-  mongorestore --gzip --archive=$file \
-  && echo "part1" \
-  && count=`mongo $db_name --eval "printjson(db.$filename.count());" --quiet` \
-  && echo "$count" \
-  && index=`mongo $db_name --eval "printjson(db.$filename.getIndexes());" --quiet` \
-  && echo "$index" \
-  && echo "$file, $count,$index" \
-  && echo "$file, $count,$index" >> $output_file \
-  && mongo $db_name --eval "db.$filename.drop()"
+mongo $db_name --eval "printjson(db.2019_W14_Twitter_Australia.count());" --quiet
+mongo $db_name --eval "printjson(db.2019_W14_Twitter_Australia.getIndexes());" --quiet
 
-  #echo $ind | tr ' ' ','
-  #echo ${ind// /,}
-done
+
+
+# for file in $folder/*mongodb_2019_W14_Twitter_Australia*.gz; do
+#   filename=$(basename -- "$file")
+#   filename="${filename%.*}"
+#   filename="${filename#*mongodb_}"
+#   mongorestore --gzip --archive=$file \
+#   && echo "part1" \
+#   && count=`mongo $db_name --eval "printjson(db.$filename.count());" --quiet` \
+#   && echo "$count" \
+#   && index=`mongo $db_name --eval "printjson(db.$filename.getIndexes());" --quiet` \
+#   && echo "$index" \
+#   && echo "$file, $count,$index" \
+#   && echo "$file, $count,$index" >> $output_file \
+#   && mongo $db_name --eval "db.$filename.drop()"
+#
+#   #echo $ind | tr ' ' ','
+#   #echo ${ind// /,}
+# done
