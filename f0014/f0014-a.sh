6c77811f7e5d15b5cd64d3877a3233dc321c00fc
Chris <chrislishuanghong@gmail.com>
2019-07-05 21:19:38 +1000

    debugging

--- f0014/f0014-a.sh
+++ f0014/f0014-a.sh
@@ -10,7 +10,10 @@ for file in $folder/*mongodb_2019_W14_Twitter_Australia*.gz; do
   filename="${filename#*mongodb_}"
   mongorestore --gzip --archive=$file \
   && count=`mongo $db_name --eval "printjson(db.$filename.count());" --quiet` \
+  && echo "$count" \
   && index=`mongo $db_name --eval "printjson(db.$filename.getIndexes());" --quiet` \
+  && echo "$index" \
+  && echo "$file, $count,$index"
   && echo "$file, $count,$index" >> $output_file \
   && mongo $db_name --eval "db.$filename.drop()"
 
