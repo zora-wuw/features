4ba0e7be213cdc4f0efc5abdc2d3bdfcd5a6e261
Chris <chrislishuanghong@gmail.com>
2019-07-05 18:48:29 +1000

    update test version

--- f0014/f0014-a.sh
+++ f0014/f0014-a.sh
@@ -7,12 +7,12 @@ output_file=$3
 for file in $folder/*2019_W14_Twitter_Australia*.gz; do
   filename=$(basename -- "$file")
   filename="${filename%.*}"
-  filename="${filename#*mongodb_}"
-  mongorestore --gzip --archive=$file \
-  && count=`mongo $db_name --eval "printjson(db.$filename.count());" --quiet` \
-  && index=`mongo $db_name --eval "printjson(db.$filename.getIndexes());" --quiet` \
+  subfilename="${filename#*mongodb_}"
+  mongorestore --gzip --archive=$subfilename \
+  && count=`mongo $db_name --eval "printjson(db.$subfilename.count());" --quiet` \
+  && index=`mongo $db_name --eval "printjson(db.$subfilename.getIndexes());" --quiet` \
   && echo "$filename, $count,$index" >> $output_file \
-  && mongo $db_name --eval "db.$filename.drop()"
+  && mongo $db_name --eval "db.$subfilename.drop()"
 
   #echo $ind | tr ' ' ','
   #echo ${ind// /,}
