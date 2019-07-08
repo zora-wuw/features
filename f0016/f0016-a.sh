593489cdb61724042e6c414781f230d17b97a385
Chris <chrislishuanghong@gmail.com>
2019-07-08 10:26:08 +1000

    add argument

--- f0016/f0016-a.sh
+++ f0016/f0016-a.sh
@@ -9,6 +9,6 @@ for file in $folder/mongodb_2019_W14_Twitter_Australia*.gz; do
   filename="${filename%.*}"
   filename="${filename#*mongo_}"
   mongorestore --gzip --archive=$file \
-  && python3 $py_name $filename \
+  && python3 $py_name $filename $db_name\
   && mongo $db_name --eval "db.$filename.drop()"
 done
