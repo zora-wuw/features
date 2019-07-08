b2333b4bee4c3b724fdf4e99f0d7fcf7c8a2a4a8
Chris <chrislishuanghong@gmail.com>
2019-07-08 10:35:36 +1000

    fix error

--- f0016/f0016-a.sh
+++ f0016/f0016-a.sh
@@ -7,7 +7,7 @@ py_name=$3
 for file in $folder/mongodb_2019_W14_Twitter_Australia*.gz; do
   filename=$(basename -- "$file")
   filename="${filename%.*}"
-  filename="${filename#*mongo_}"
+  filename="${filename#*mongodb_}"
   mongorestore --gzip --archive=$file \
   && python3 $py_name $filename $db_name\
   && mongo $db_name --eval "db.$filename.drop()"
