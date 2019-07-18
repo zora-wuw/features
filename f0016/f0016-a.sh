5a35177b309c05b728c01226593da982766037f8
Chris <chrislishuanghong@gmail.com>
2019-07-18 17:37:22 +1000

    update: change restore database name

--- f0016/f0016-a.sh
+++ f0016/f0016-a.sh
@@ -6,6 +6,7 @@ py_name=$3
 prefix=$4
 latest_year=$5
 latest_week=$6
+new_db_name=$7
 
 for file in $folder/"$prefix"*.gz; do
   filename=$(basename -- "$file")
@@ -15,17 +16,17 @@ for file in $folder/"$prefix"*.gz; do
   week=${left%_T*}
   if [[ $year -eq $latest_year ]]; then
     if [[ $week -gt $latest_week ]]; then
-      mongorestore --gzip --archive=$file
+      mongorestore --gzip --archive=$file --nsFrom "${db_name}.*" --nsTo "${new_db_name}.*"
       && python3 $py_name $filename \
       && echo $filename > geoname_latest_collection.txt
-      && mongo $db_name --eval "db.getCollection('$filename').drop()"
+      && mongo $new_db_name --eval "db.getCollection('$filename').drop()"
     fi
   else
     if [[ $year -gt $latest_year ]]; then
-      mongorestore --gzip --archive=$file
+      mongorestore --gzip --archive=$file --nsFrom "${db_name}.*" --nsTo "${new_db_name}.*"
       && python3 $py_name $filename \
       && echo $filename > geoname_latest_collection.txt
-      && mongo $db_name --eval "db.getCollection('$filename').drop()"
+      && mongo $new_db_name --eval "db.getCollection('$filename').drop()"
     fi
   fi
 done
