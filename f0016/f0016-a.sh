cdbc1550932af2ddd37a3387b6a7bbb3b3c93e10
Chris <chrislishuanghong@gmail.com>
2019-07-19 13:52:13 +1000

    test version

--- f0016/f0016-a.sh
+++ f0016/f0016-a.sh
@@ -4,11 +4,18 @@ folder=$1
 db_name=$2
 py_name=$3
 prefix=$4
-new_db_name=$5
+latest_year=$5
+latest_week=$6
+new_db_name=$7
 
 for file in $folder/"$prefix"*.gz; do
   filename=$(basename -- "$file")
   filename="${filename%.*}"
+  year=${filename:0:4}
+  left=${filename#*_W}
+  week=${left%_T*}
+  echo $year
+  echo $week
   mongorestore --gzip --archive=$file --nsFrom "${db_name}.*" --nsTo "${new_db_name}.*" \
   && python3 $py_name $filename $new_db_name \
   && echo $filename > geoname_latest_collection.txt \
