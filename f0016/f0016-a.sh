807c662298d61c3688348cd601c3dd3ba1fdd07d
Chris <chrislishuanghong@gmail.com>
2019-07-19 15:45:51 +1000

    update

--- f0016/f0016-a.sh
+++ f0016/f0016-a.sh
@@ -2,6 +2,11 @@
 
 folder=$1
 prefix=$2
+db_name=$3
+new_db_name=$4
+py_name=$5
+latest_year=$6
+latest_week=$7
 
 for file in $folder/"$prefix"*.gz; do
   echo $file
@@ -10,21 +15,19 @@ for file in $folder/"$prefix"*.gz; do
   year=${filename:0:4}
   left=${filename#*_W}
   week=${left%_T*}
-  echo $year
-  echo $week
-  # if [ "$year" -eq "2018" ]; then
-  #   if [ "$week" -gt "51" ]; then
-  #     mongorestore --gzip --archive=$file --nsFrom "${db_name}.*" --nsTo "${new_db_name}.*" \
-  #     && python3 $py_name $filename $new_db_name \
-  #     && echo $filename > geoname_latest_collection.txt \
-  #     && mongo $new_db_name --eval "db.getCollection('$filename').drop()"
-  #   fi
-  # else
-  #   if [ "$year" -gt "2018" ]; then
-  #     mongorestore --gzip --archive=$file --nsFrom "${db_name}.*" --nsTo "${new_db_name}.*" \
-  #     && python3 $py_name $filename $new_db_name \
-  #     && echo $filename > geoname_latest_collection.txt \
-  #     && mongo $new_db_name --eval "db.getCollection('$filename').drop()"
-  #   fi
-  # fi
+  if [ "$year" -eq "$latest_year" ]; then
+    if [ "$week" -gt "$latest_week" ]; then
+      mongorestore --gzip --archive=$file --nsFrom "${db_name}.*" --nsTo "${new_db_name}.*" \
+      && python3 $py_name $filename $new_db_name \
+      && echo $filename > geoname_latest_collection.txt \
+      && mongo $new_db_name --eval "db.getCollection('$filename').drop()"
+    fi
+  else
+    if [ "$year" -gt "$latest_year" ]; then
+      mongorestore --gzip --archive=$file --nsFrom "${db_name}.*" --nsTo "${new_db_name}.*" \
+      && python3 $py_name $filename $new_db_name \
+      && echo $filename > geoname_latest_collection.txt \
+      && mongo $new_db_name --eval "db.getCollection('$filename').drop()"
+    fi
+  fi
 done
