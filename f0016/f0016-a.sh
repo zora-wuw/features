225341b09925a0a74e09ffb063a242757a8ab464
Chris <chrislishuanghong@gmail.com>
2019-07-19 14:57:22 +1000

    test version

--- f0016/f0016-a.sh
+++ f0016/f0016-a.sh
@@ -14,15 +14,15 @@ for file in $folder/"$prefix"*.gz; do
   year=${filename:0:4}
   left=${filename#*_W}
   week=${left%_T*}
-  if [ $year -eq $latest_year ]; then
-    if [ $week -gt $latest_week ]; then
+  if [ "$year" -eq "$latest_year" ]; then
+    if [ "$week" -gt "$latest_week" ]; then
       mongorestore --gzip --archive=$file --nsFrom "${db_name}.*" --nsTo "${new_db_name}.*" \
       && python3 $py_name $filename $new_db_name \
       && echo $filename > geoname_latest_collection.txt \
       && mongo $new_db_name --eval "db.getCollection('$filename').drop()"
     fi
   else
-    if [ $year -gt $latest_year ]; then
+    if [ "$year" -gt "$latest_year" ]; then
       mongorestore --gzip --archive=$file --nsFrom "${db_name}.*" --nsTo "${new_db_name}.*" \
       && python3 $py_name $filename $new_db_name \
       && echo $filename > geoname_latest_collection.txt \
