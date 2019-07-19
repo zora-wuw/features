048a4c2ab54ba671bef76c21c937e903abd1fb50
Chris <chrislishuanghong@gmail.com>
2019-07-19 11:51:54 +1000

    test version:f16-a

--- f0016/f0016-a.sh
+++ f0016/f0016-a.sh
@@ -4,29 +4,13 @@ folder=$1
 db_name=$2
 py_name=$3
 prefix=$4
-latest_year=$5
-latest_week=$6
-new_db_name=$7
+new_db_name=$5
 
 for file in $folder/"$prefix"*.gz; do
   filename=$(basename -- "$file")
   filename="${filename%.*}"
-  year="${filename:0:4}"
-  left="${filename#*_W}"
-  week="${left%_T*}"
-  if [[ $year -eq $latest_year ]]; then
-    if [[ $week -gt $latest_week ]]; then
-      mongorestore --gzip --archive=$file --nsFrom "${db_name}.*" --nsTo "${new_db_name}.*" \
-      && python3 $py_name $filename $new_db_name \
-      && echo $filename > geoname_latest_collection.txt \
-      && mongo $new_db_name --eval "db.getCollection('$filename').drop()"
-    fi
-  else
-    if [[ $year -gt $latest_year ]]; then
-      mongorestore --gzip --archive=$file --nsFrom "${db_name}.*" --nsTo "${new_db_name}.*" \
-      && python3 $py_name $filename $new_db_name \
-      && echo $filename > geoname_latest_collection.txt \
-      && mongo $new_db_name --eval "db.getCollection('$filename').drop()"
-    fi
-  fi
+  mongorestore --gzip --archive=$file --nsFrom "${db_name}.*" --nsTo "${new_db_name}.*" \
+  && python3 $py_name $filename $new_db_name \
+  && echo $filename > geoname_latest_collection.txt \
+  && mongo $new_db_name --eval "db.getCollection('$filename').drop()"
 done
