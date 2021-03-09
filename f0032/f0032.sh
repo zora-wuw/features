1bac1b681bf0576ce49116dec830e7e3aef51dd1
Chris <chrislishuanghong@gmail.com>
2021-03-09 16:08:21 +1100

    update: fix string equal error

--- f0032/f0032.sh
+++ f0032/f0032.sh
@@ -12,7 +12,7 @@ swift_project_name=$(awk -F "=" '/OS-Project-Name/ {print $2}' $config_file)
 swift_region_name=$(awk -F "=" '/OS-Region-Name/ {print $2}' $config_file)
 swift_user_domain_name=$(awk -F "=" '/OS-User-Domain-Name/ {print $2}' $config_file)
 
-if [$action == "download"]; then
+if [ "$action" == "download" ]; then
   for file in $file_array; do
     swift --os-username $swift_username --os-password $swift_pwd --os-auth-url https://keystone.rc.nectar.org.au:5000/v3/ --os-region-name $swift_region_name --os-project-id $swift_project_id --os-project-name $swift_project_name --os-user-domain-name $swift_user_domain_name --os-identity-api-version 3 download $container_name $file
   done
