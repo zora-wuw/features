edaa9a0fb03fb8ed6ba05e51bc9f25623e33c56d
Chris <chrislishuanghong@gmail.com>
2021-03-09 16:00:22 +1100

    ad argument for action:download & upload

--- f0032/f0032.sh
+++ f0032/f0032.sh
@@ -3,6 +3,7 @@
 config_file=$1
 container_name=$2
 file_array=$3
+action=$4
 
 swift_username=$(awk -F "=" '/Username/ {print $2}' $config_file)
 swift_pwd=$(awk -F "=" '/Psword/ {print $2}' $config_file)
@@ -11,6 +12,12 @@ swift_project_name=$(awk -F "=" '/OS-Project-Name/ {print $2}' $config_file)
 swift_region_name=$(awk -F "=" '/OS-Region-Name/ {print $2}' $config_file)
 swift_user_domain_name=$(awk -F "=" '/OS-User-Domain-Name/ {print $2}' $config_file)
 
-for file in $file_array; do
-  swift --os-username $swift_username --os-password $swift_pwd --os-auth-url https://keystone.rc.nectar.org.au:5000/v3/ --os-region-name $swift_region_name --os-project-id $swift_project_id --os-project-name $swift_project_name --os-user-domain-name $swift_user_domain_name --os-identity-api-version 3 download $container_name $file
-done
+if [$action == "download"]; then
+  for file in $file_array; do
+    swift --os-username $swift_username --os-password $swift_pwd --os-auth-url https://keystone.rc.nectar.org.au:5000/v3/ --os-region-name $swift_region_name --os-project-id $swift_project_id --os-project-name $swift_project_name --os-user-domain-name $swift_user_domain_name --os-identity-api-version 3 download $container_name $file
+  done
+else
+  for file in $file_array; do
+    swift --os-username $swift_username --os-password $swift_pwd --os-auth-url https://keystone.rc.nectar.org.au:5000/v3/ --os-region-name $swift_region_name --os-project-id $swift_project_id --os-project-name $swift_project_name --os-user-domain-name $swift_user_domain_name --os-identity-api-version 3 upload $container_name $file
+  done
+fi
