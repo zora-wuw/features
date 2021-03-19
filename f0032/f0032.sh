d6e97271d453afc8213e0dba1ce4b6b3f9f6bb45
Chris <chrislishuanghong@gmail.com>
2021-03-19 14:47:07 +1100

    add download_prefix

--- f0032/f0032.sh
+++ f0032/f0032.sh
@@ -11,13 +11,25 @@ swift_project_id=$(awk -F "=" '/OS-Project-ID/ {print $2}' $config_file)
 swift_project_name=$(awk -F "=" '/OS-Project-Name/ {print $2}' $config_file)
 swift_region_name=$(awk -F "=" '/OS-Region-Name/ {print $2}' $config_file)
 swift_user_domain_name=$(awk -F "=" '/OS-User-Domain-Name/ {print $2}' $config_file)
+swift_auth_url=$(awk -F "=" '/OS-Auth-URL/ {print $2}' $config_file)
+swift_auth_url=$(awk -F "=" '/OS-Auth-URL/ {print $2}' $config_file)
+swift_temp_url_key=$(awk -F "=" '/Temp-URL-Key/ {print $2}' $config_file)
+swift_object_url=$(awk -F "=" '/Object-URL/ {print $2}' $config_file)
+
 
 if [ "$action" == "download" ]; then
   for file in $file_array; do
-    swift --os-username $swift_username --os-password $swift_pwd --os-auth-url https://keystone.rc.nectar.org.au:5000/v3/ --os-region-name $swift_region_name --os-project-id $swift_project_id --os-project-name $swift_project_name --os-user-domain-name $swift_user_domain_name --os-identity-api-version 3 download $container_name $file
+    swift --os-username $swift_username --os-password $swift_pwd --os-auth-url $swift_auth_url --os-region-name $swift_region_name --os-project-id $swift_project_id --os-project-name $swift_project_name --os-user-domain-name $swift_user_domain_name --os-identity-api-version 3 download $container_name $file
   done
-else
+elif [ "$action" == "download_prefix" ]; then
   for file in $file_array; do
-    swift --os-username $swift_username --os-password $swift_pwd --os-auth-url https://keystone.rc.nectar.org.au:5000/v3/ --os-region-name $swift_region_name --os-project-id $swift_project_id --os-project-name $swift_project_name --os-user-domain-name $swift_user_domain_name --os-identity-api-version 3 upload $container_name $file
+    swift --os-username $swift_username --os-password $swift_pwd --os-auth-url $swift_auth_url --os-region-name $swift_region_name --os-project-id $swift_project_id --os-project-name $swift_project_name --os-user-domain-name $swift_user_domain_name --os-identity-api-version 3 download --prefix $file $container_name
   done
+elif [ "$action" == "upload" ]; then
+  for file in $file_array; do
+    swift --os-username $swift_username --os-password $swift_pwd --os-auth-url $swift_auth_url --os-region-name $swift_region_name --os-project-id $swift_project_id --os-project-name $swift_project_name --os-user-domain-name $swift_user_domain_name --os-identity-api-version 3 upload $container_name $file
+  done
+elif [ "$action" == "tempurl" ]; then
+  expire_seconds=$5
+  swift tempurl GET $expire_seconds ${swift_object_url}${container_name}/${file_array} $swift_temp_url_key --os-username $swift_username --os-password $swift_pwd --os-auth-url $swift_auth_url --os-region-name $swift_region_name --os-project-id $swift_project_id --os-project-name $swift_project_name --os-user-domain-name $swift_user_domain_name --os-identity-api-version 3
 fi
