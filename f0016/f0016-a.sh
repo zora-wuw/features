0c1f9689fe7fa30123b9a555044bd8643f1b86e1
Chris <chrislishuanghong@gmail.com>
2021-08-03 10:29:46 +1000

    update: add mongo ip & port

--- f0016/f0016-a.sh
+++ f0016/f0016-a.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# declare port is an integer
+declare -i port
+
 folder=$1
 prefix=$2
-db_name=$3
-new_db_name=$4
-py_name=$5
-latest_year=$6
-latest_week=$7
+ip=$3
+port=$4
+db_name=$5
+new_db_name=$6
+py_name=$7
+latest_year=$8
+latest_week=$9
 
 for file in $folder/"$prefix"*.gz; do
   filename=$(basename -- "$file")
@@ -16,17 +21,17 @@ for file in $folder/"$prefix"*.gz; do
   week=${left%_T*}
   if [ "$year" -eq "$latest_year" ]; then
     if [ "$week" -gt "$latest_week" ]; then
-      mongorestore --gzip --archive=$file --nsFrom "${db_name}.*" --nsTo "${new_db_name}.*" \
+      mongorestore --host $ip --port $port --gzip --archive=$file --nsFrom "${db_name}.*" --nsTo "${new_db_name}.*" \
       && python3 $py_name $filename $new_db_name \
       && echo $filename > geoname_latest_collection.txt \
-      && mongo $new_db_name --eval "db.getCollection('$filename').drop()"
+      && mongo --host $ip --port $port $new_db_name --eval "db.getCollection('$filename').drop()"
     fi
   else
     if [ "$year" -gt "$latest_year" ]; then
-      mongorestore --gzip --archive=$file --nsFrom "${db_name}.*" --nsTo "${new_db_name}.*" \
+      mongorestore --host $ip --port $port --gzip --archive=$file --nsFrom "${db_name}.*" --nsTo "${new_db_name}.*" \
       && python3 $py_name $filename $new_db_name \
       && echo $filename > geoname_latest_collection.txt \
-      && mongo $new_db_name --eval "db.getCollection('$filename').drop()"
+      && mongo --host $ip --port $port $new_db_name --eval "db.getCollection('$filename').drop()"
     fi
   fi
 done
